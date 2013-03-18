954c2b5d39b06ce82a02fa68e75dd7f62ca75035
kaustubh <kausrails@gmail.com>
2013-03-18 20:05:35 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Practo::Application.config.secret_token = '4d74599a742da60ad449012681f4ed0f8d51ad3306742844e96c98dbc6bda8f350837471750cbc06596b97b36cc42cd2d585f21df2e3c5653da1c5b7e85454e5'
